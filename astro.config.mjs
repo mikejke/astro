97076623d2d2b43983d8daa0281331b320227001
mikejke <mikejke99@gmail.com>
2023-04-02 14:24:01 +0300

    feat: add site url to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://mikejke.github.io/landing",
   integrations: [solidJs(), image()],
   experimental: {
     assets: true,
